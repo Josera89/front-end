f60f328258381290f07a7aa2aff6fdc08e5d90c4
Jose Zuniga <jramon.zu@gmail.com>
2018-08-14 22:59:15 -0500

    front

---
+++ ChangeLog.md
@@ -0,0 +1,11 @@
+# [8.0.0](http://gitstrap.com/strapmobile/NativeStarterPro-with-backend-integration/blob/v8.0.0/ChangeLog.md)
+
+### Enhancement Features
+
+- Upgraded NativeBase from 2.1.0-rc.2 to 2.3.1
+- Upgraded React from 15.4.0 to 16.0.0-alpha.12
+- Upgraded React Native from 0.42.3 to 0.48.4
+- Upgraded Expo from 15.1.0 to 21.0.2
+- Upgraded Mobx from 3.1.0 to 3.3.1
+- Upgraded Mobx-React from 4.1.0 4.3.3
+- Upgraded Color from 0.11.3 to 1.0.3
---
+++ js/components/sideBar/index.js
@@ -0,0 +1,100 @@
+
+import React, { Component } from 'react';
+import { Text, Icon, List, ListItem, Content, Thumbnail ,Left, Body,Right } from 'native-base';
+import { Actions } from 'react-native-router-flux';
+import AuthStore from '../../stores/EntityStore/AuthStore.js';
+
+import styles from './style';
+
+import AppViewStore from '../../stores/ViewStore/AppViewStore.js';
+
+const logo = require('../../../images/truck.png');
+
+class SideBar extends Component { // eslint-disable-line
+  render() { // eslint-disable-line class-methods-use-this
+    return (
+      <Content style={{ backgroundColor: '#252A30' }} >
+        <Thumbnail  style={{ alignSelf: 'center', width: 200, height: 200, marginTop: 20, marginBottom: 15, resizeMode: 'contain' }} circular source={logo} />
+          <ListItem
+            button
+            onPress={
+              () => {
+                AppViewStore.drawerOpened = false;
+                if (AppViewStore.routeStack2[AppViewStore.routeStack2.length - 1].key !== 'home') {
+                  Actions.home();
+                }
+              }
+            }
+            icon style={styles.links}
+          >
+            <Left>
+             <Icon active name="home" />
+            </Left>
+            <Body>
+              <Text >Home</Text>
+            </Body>
+            <Right />
+          </ListItem>
+          <ListItem
+            button
+            onPress={
+              () => {
+                AppViewStore.drawerOpened = false;
+                Actions.blankPage();
+              }
+            }
+            icon style={styles.links}
+          >
+            <Left>
+               <Icon active name="chatboxes" />
+            </Left>
+            <Body>
+               <Text>Blank Page</Text>
+            </Body>
+            <Right />
+          </ListItem>
+
+
+          <ListItem
+            button
+            onPress={
+              () => {
+                AppViewStore.drawerOpened = false;
+                Actions.profileConfig();
+              }
+            }
+            icon style={styles.links}
+          >
+            <Left>
+               <Icon active name="settings" />
+            </Left>
+            <Body>
+               <Text>Configuración</Text>
+            </Body>
+            <Right />
+          </ListItem>
+
+          <ListItem
+            button
+            onPress={
+              () => {
+              AppViewStore.drawerOpened = false;
+              AuthStore.unsetUser()
+              }
+            }
+            icon style={styles.links}
+          >
+            <Left>
+               <Icon active name="exit" />
+            </Left>
+            <Body>
+               <Text>Exit</Text>
+            </Body>
+            <Right />
+          </ListItem>
+      </Content>
+    );
+  }
+}
+
+export default SideBar;
